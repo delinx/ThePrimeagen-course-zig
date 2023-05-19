be6fb6753d73509e0779e5c6c50b3bbf4a97affd
Mihails M <mail@del.cx>
2023-05-19 13:06:35 +0100

    binary search

--- 1-search/src/main.zig
+++ 1-search/src/main.zig
@@ -2,36 +2,60 @@ const std = @import("std");
 
 // o(n)
 // linear search
-pub fn linear_search(heystack: []const u8, needle: u8) usize {
-    var index: usize = undefined;
+pub fn linear_search(heystack: []const u8, needle: u8) ?usize {
     for (heystack, 0..) |letter, idx| {
         if (letter == needle) {
-            index = idx;
+            return idx;
         }
     }
-    return index;
+    return null;
 }
 
-//
+// O(logN)
 // binary search
+pub fn binary_search(heystack: []const i32, needle: i32) ?usize {
+    var low: f32 = 0;
+    var high: f32 = @intToFloat(f32, heystack.len);
+
+    while (low < high) {
+        const middle: f32 = @floor(low + (high - low) / 2);
+        const value: i32 = heystack[@floatToInt(usize, middle)];
+
+        if (value == needle) {
+            return @floatToInt(usize, middle);
+        }
+
+        if (value > needle) {
+            high = middle;
+        }
+
+        if (value < needle) {
+            low = middle + 1;
+        }
+    }
+
+    return null;
+}
 
 pub fn main() !void {
     const text_example_1 = "Zig is the best!";
+    const text_example_2 = [_]i32{ 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
     // linear search
-    std.debug.print("Linear Search\n{s}\n{} Z\n{} i\n{} !\n", .{
+    std.debug.print("> Linear Search\n{s}\n'Z' -> {?}\n'i' -> {?}\n'!' -> {?}\n'A' -> {?}\n\n", .{
         text_example_1,
         linear_search(text_example_1, 'Z'),
         linear_search(text_example_1, 'i'),
         linear_search(text_example_1, '!'),
+        linear_search(text_example_1, 'A'),
     });
 
-    //
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    // binary search
+    std.debug.print("> Binary Search\n{any}\n'1' -> {?}\n'777' -> {?}\n'32' -> {?}\n'512' -> {?}\n\n", .{
+        text_example_2,
+        binary_search(&text_example_2, 1),
+        binary_search(&text_example_2, 777),
+        binary_search(&text_example_2, 32),
+        binary_search(&text_example_2, 512),
+    });
 }
